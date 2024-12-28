e513448b5c37a5cb3f2f81521de27e6810909e34
Diego Ramirez Almaguer <diegoramalm456@gmail.com>
2024-12-28 17:10:59 -0600

    Se agrego clase email

---
+++ Mail/Email.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mail
+{
+    public class Email
+    {
+        private MailMessage email;
+        private SmtpClient server;
+
+        public Email()
+        {
+            server = new SmtpClient();
+            server.Credentials = new NetworkCredential("portafoliodra@gmail.com", "portafolio123");
+            server.EnableSsl = true;
+            server.Port = 587;
+            server.Host = "smtp.gmail.com";
+        }
+
+        public void generarCorreo(string Asunto, string Mensaje, string Email)
+        {
+            email = new MailMessage();
+            email.From = new MailAddress(Email);
+            email.To.Add("alfonsodiego048@gmail.com");
+            email.Subject = Asunto;
+            email.Body = Mensaje;
+        }
+
+        public void enviarCorreo()
+        {
+            server.Send(email);
+        }
+    }
+}
